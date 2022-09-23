cf9e492c982be9e9cc5012279314679008233b71
franSborges <borgesfran988@gmail.com>
2022-09-23 13:53:33 -0300

    :feat: service show product created :construction_worker:

---
+++ src/services/ShowProductService.ts
@@ -0,0 +1,22 @@
+import Product from "@modules/products/typeorm/entities/Product";
+import { ProductRepository } from "@modules/products/typeorm/repositories/ProductsRepository";
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+
+interface IRequest {
+  id: string;
+}
+
+class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    
+    const product = productsRepository.findOne(id);
+
+    if (!product) throw new AppError('Product not found')
+
+    return product;
+  }
+}
+
+export default ShowProductService;
