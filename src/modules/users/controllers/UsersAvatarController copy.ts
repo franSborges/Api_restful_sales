200004db7294a560f2f39be0fb3c92548e66a94d
franSborges <borgesfran988@gmail.com>
2022-09-27 11:25:26 -0300

    :fix: user controller avatar :white_check_mark:

---
+++ src/modules/users/controllers/UsersAvatarController copy.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+export default class UserAvatarController {
+  public async update(req: Request, res: Response): Promise<Response> {
+    const updateAvatar = new UpdateUserAvatarService();
+
+    const user = updateAvatar.execute({
+      user_id: req.user.id,
+      avatarFilename: req.file.filename,
+    });
+
+    return res.json(user);
+  }
+}
+ 41  
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,32 +1,33 @@
-import User from "../typeorm/entities/User";
-import path from "path";
-import { UserRepository } from "@modules/users/typeorm/repositories/UsersRepository";
-import AppError from "@shared/errors/AppError";
-import { getCustomRepository } from "typeorm";
-import uploadConfig from "@config/upload";
+import AppError from '@shared/errors/AppError';
+import path from 'path';
 import fs from 'fs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import uploadConfig from '@config/upload';
 
 interface IRequest {
   user_id: string;
   avatarFilename: string;
-  password: string;
 }
 
 class UpdateUserAvatarService {
-  public async execute({ user_id, avatarFilename }: IRequest): Promise<User | undefined> {
-    const usersRepository = getCustomRepository(UserRepository);
-    
-   const user = await usersRepository.findById(user_id);
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
 
-   if (!user) throw new AppError('User not found');
-   
-   if (!user.avatar){
-    const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+    const user = await usersRepository.findById(user_id);
 
-    const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+    if (!user) {
+      throw new AppError('User not found.');
+    }
 
-    if (userAvatarFileExists) {
-      await fs.promises.unlink(userAvatarFilePath)
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
     }
 
     user.avatar = avatarFilename;
@@ -34,7 +35,6 @@ class UpdateUserAvatarService {
     await usersRepository.save(user);
 
     return user;
-   }
   }
 }
 
